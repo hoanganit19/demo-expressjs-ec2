09e8073fdab398c8c34ee05d178e9105de920278
Hoang An <hoangan@fullstack.edu.vn>
2025-09-14 15:52:51 +0700

    Update code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", async (req, res) => {
-  const response = await fetch(`http://jsonplaceholder.typicode.com/todos/1`);
+  const response = await fetch(`http://${req.headers.host}/data`);
   const data = await response.json();
   res.json(data);
 });
@@ -12,6 +12,12 @@ app.get("/demo", async (req, res) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
   const data = await response.json();
   res.json(data);
+});
+
+app.get("/data", (req, res) => {
+  res.json({
+    title: "Unicode",
+  });
 });
 
 app.listen(port, () => {
